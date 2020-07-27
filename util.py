8559587b85c83881a32333cbe8e161059ec37eea
Artperkitny <Artperkitny@gmail.com>
2020-07-26 23:40:56 -0400

    init commit

---
+++ util.py
@@ -0,0 +1,8 @@
+import contextily as ctx
+
+def add_basemap(ax, zoom, url='http://tile.stamen.com/terrain/tileZ/tileX/tileY.png'):
+    xmin, xmax, ymin, ymax = ax.axis()
+    basemap, extent = ctx.bounds2img(xmin, ymin, xmax, ymax, zoom=zoom, url=url)
+    ax.imshow(basemap, extent=extent, interpolation='bilinear')
+    ax.axis((xmin, xmax, ymin, ymax))
+
