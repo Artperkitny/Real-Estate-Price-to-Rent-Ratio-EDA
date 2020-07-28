b4cdb49821ed59e29d0a26dc04865765d30c2dba
Artperkitny <Artperkitny@gmail.com>
2020-07-27 23:01:52 -0400

    added more EDA notebooks

--- util.py
+++ util.py
@@ -1,4 +1,6 @@
 import contextily as ctx
+import json
+
 
 def add_basemap(ax, zoom, url='http://tile.stamen.com/terrain/tileZ/tileX/tileY.png'):
     xmin, xmax, ymin, ymax = ax.axis()
@@ -6,3 +8,19 @@ def add_basemap(ax, zoom, url='http://tile.stamen.com/terrain/tileZ/tileX/tileY.
     ax.imshow(basemap, extent=extent, interpolation='bilinear')
     ax.axis((xmin, xmax, ymin, ymax))
 
+
+def export_chart_json(df):
+    output = {
+        "labels": list(df.iloc[0]),
+        "datasets": []
+    }
+
+    for i, row in df[1:].iterrows():
+        values_dict = {}
+        values_dict["label"] = "{}".format(i)
+        values_dict["data"] = [round(n,1) for n in list(row)]
+        values_dict["fill"] = False
+        
+        output["datasets"].append(values_dict)
+
+    return json.dumps(output)
